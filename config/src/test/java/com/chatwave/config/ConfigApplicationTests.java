0307e550b34a90ab64fdd010336198929d32f59c
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-07 09:53:47 +0200

    create config server

---
+++ config/src/test/java/com/chatwave/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatwave.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
