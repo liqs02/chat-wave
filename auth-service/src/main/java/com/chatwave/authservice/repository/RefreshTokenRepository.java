e8e7c4e5f4d3c8ab4184f536c054e7f98585ce4e
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-12 22:19:50 +0200

    add deleteExpired method to refreshTokenRepository with test

--- auth-service/src/main/java/com/chatwave/authservice/repository/RefreshTokenRepository.java
+++ auth-service/src/main/java/com/chatwave/authservice/repository/RefreshTokenRepository.java
@@ -2,9 +2,16 @@ package com.chatwave.authservice.repository;
 
 import com.chatwave.authservice.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID>  {}
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID>  {
+    @Modifying
+    @Query("DELETE FROM RefreshToken rt WHERE rt.expirationAt <= CURRENT_DATE")
+    void deleteExpired();
+}
+
--- auth-service/src/test/java/com/chatwave/authservice/repository/RefreshTokenRepositoryTest.java
+++ auth-service/src/test/java/com/chatwave/authservice/repository/RefreshTokenRepositoryTest.java
@@ -2,12 +2,16 @@ package com.chatwave.authservice.repository;
 
 import com.chatwave.authservice.domain.RefreshToken;
 import com.chatwave.authservice.domain.User;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +22,10 @@ public class RefreshTokenRepositoryTest {
     private RefreshTokenRepository repository;
     @Autowired
     private UserRepository userRepository;
+    @BeforeEach
+    void setUp() {
+        repository.deleteAll();
+    }
 
     @Test
     @DisplayName("should save and find refresh token by id")
@@ -47,5 +55,37 @@ public class RefreshTokenRepositoryTest {
 
         assertNotNull(foundToken.getUser());
         assertEquals(1, foundToken.getUser().getId());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("should delete expired tokens")
+    public void deleteExpired() throws NoSuchFieldException, IllegalAccessException {
+        var user = new User();
+        user.setId(1);
+        user.setPassword("pass");
+        userRepository.save(user);
+
+        // create correct token
+        var correctToken = new RefreshToken();
+        correctToken.setUser(user);
+
+        // create expired refresh token
+        var expiredToken = new RefreshToken();
+        expiredToken.setUser(user);
+
+        var expiredField = RefreshToken.class.getDeclaredField("expirationAt");
+        expiredField.setAccessible(true);
+        expiredField.set(expiredToken, LocalDate.now().minusDays(1));
+
+        repository.saveAll(
+                List.of(correctToken, expiredToken)
+        );
+
+        repository.deleteExpired();
+
+        var tokenList = repository.findAll();
+        assertEquals(1, tokenList.size());
+        assertEquals(correctToken.getId(), tokenList.get(0).getId());
     }
 }
