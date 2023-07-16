3aa8d07673b7b145f8f58f6d245581e4841966ac
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-16 12:25:16 +0200

    add sessionRepository methods with tests. Create sessionScheduler to delete tokens from expired sessions.

--- auth-service/src/main/java/com/chatwave/authservice/repository/SessionRepository.java
+++ auth-service/src/main/java/com/chatwave/authservice/repository/SessionRepository.java
@@ -1,14 +1,28 @@
 package com.chatwave.authservice.repository;
 
-import com.chatwave.authservice.domain.Session;
+import com.chatwave.authservice.domain.session.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long>  {
-    Optional<Session> findByRefreshToken(String refreshToken);
     Optional<Session> findByAccessToken(String accessToken);
+
+    Optional<Session> findByRefreshToken(String refreshToken);
+
+    Optional<Session> findNotExpiredByIdAndUserId(Long sessionId, Integer userId); // TODO: delete if won't be used
+
+    @Query("SELECT s FROM Session s WHERE s.accessToken = ?1 AND CURRENT_TIMESTAMP < s.accessTokenExpireDate AND CURRENT_DATE < s.expireDate")
+    Optional<Session> findNotExpiredByAccessToken(String accessToken);
+
+    @Query("SELECT s FROM Session s WHERE s.expireDate <= CURRENT_DATE AND (s.accessToken != null or s.refreshToken != null)")
+    List<Session> findAllExpiredWithAccessOrRefreshToken();
+
+    @Query("SELECT s FROM Session s WHERE s.user.id = ?1 AND CURRENT_DATE < s.expireDate")
+    List<Session> findAllNotExpiredByUserId(Integer userId);
 }
 
