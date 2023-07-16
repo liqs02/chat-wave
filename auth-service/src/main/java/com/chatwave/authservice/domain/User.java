6fdaaa43126882fe18b1a3e6fb391486ade3ae71
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-16 14:00:39 +0200

    change User entity getAuthorities() method

--- auth-service/src/main/java/com/chatwave/authservice/domain/User.java
+++ auth-service/src/main/java/com/chatwave/authservice/domain/User.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
@@ -24,7 +25,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of();
     }
 
     @Override
