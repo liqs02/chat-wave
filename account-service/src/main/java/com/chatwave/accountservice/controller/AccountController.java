ba66ff2dd08b59e2645e1c509cd2cd96c969f701
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-19 20:42:11 +0200

    create AccountController

---
+++ account-service/src/main/java/com/chatwave/accountservice/controller/AccountController.java
@@ -0,0 +1,27 @@
+package com.chatwave.accountservice.controller;
+
+import com.chatwave.accountservice.domain.AccountMapper;
+import com.chatwave.accountservice.domain.dto.CreateAccountRequest;
+import com.chatwave.accountservice.domain.dto.TokenSetResponse;
+import com.chatwave.accountservice.service.AccountService;
+import jakarta.validation.Valid;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/accounts")
+@Setter(onMethod_=@Autowired)
+public class AccountController {
+    AccountService service;
+    AccountMapper mapper;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public TokenSetResponse createAccount(@Valid @RequestBody CreateAccountRequest createAccountRequest) {
+        var account = mapper.toAccount(createAccountRequest);
+        return service.createAccount(account, createAccountRequest.password());
+    }
+}
