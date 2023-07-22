ff2a5c98911a5efc0a3599d077906226a14af817
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-22 13:33:10 +0200

    handle conflict exception in createUser method

--- account-service/src/main/java/com/chatwave/accountservice/service/AccountServiceImpl.java
+++ account-service/src/main/java/com/chatwave/accountservice/service/AccountServiceImpl.java
@@ -5,16 +5,20 @@ import com.chatwave.accountservice.domain.Account;
 import com.chatwave.accountservice.domain.dto.CreateUserRequest;
 import com.chatwave.accountservice.domain.dto.TokenSetResponse;
 import com.chatwave.accountservice.repository.AccountRepository;
+import feign.FeignException;
 import jakarta.transaction.Transactional;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Service
 @Setter(onMethod_=@Autowired)
+@Slf4j
 public class AccountServiceImpl implements AccountService {
     AccountRepository repository;
     AuthService authService;
@@ -28,7 +32,12 @@ public class AccountServiceImpl implements AccountService {
         repository.save(account);
 
         var createUserRequest = new CreateUserRequest(account.getId(), password);
-        return authService.createUser(createUserRequest);
+        try {
+            return authService.createUser(createUserRequest);
+        } catch(FeignException.FeignServerException.Conflict conflict) {
+            log.warn("Inconsistency in data. Conflict when tried to create user with id " + account.getId());
+            throw new ResponseStatusException(INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
