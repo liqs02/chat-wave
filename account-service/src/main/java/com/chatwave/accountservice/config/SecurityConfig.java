d5e0186c4fcdcfcba88bad7de85e5af1967e3026
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-22 13:13:22 +0200

    create SecurityConfig

---
+++ account-service/src/main/java/com/chatwave/accountservice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.chatwave.accountservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf ->
+                        csrf.disable()
+                )
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(GET, "/actuator/health").permitAll()
+                            .requestMatchers("/error").permitAll()
+                            .requestMatchers(POST, "/accounts").permitAll()
+                            .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
