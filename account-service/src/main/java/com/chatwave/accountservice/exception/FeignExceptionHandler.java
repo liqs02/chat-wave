9a4fcb0213405b5618e3e83f56d46f17b747fe49
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-22 17:38:01 +0200

    delete contents of FeignExceptionHandler

--- account-service/src/main/java/com/chatwave/accountservice/exception/FeignExceptionHandler.java
+++ account-service/src/main/java/com/chatwave/accountservice/exception/FeignExceptionHandler.java
@@ -1,16 +1,8 @@
 package com.chatwave.accountservice.exception;
 
-import feign.FeignException;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 public class FeignExceptionHandler {
-    @ExceptionHandler({ FeignException.class }) // TODO: test it
-    public void handleFeignException(FeignException e) {
-        var statusValue = e.status();
-        throw new ResponseStatusException(HttpStatusCode.valueOf(statusValue) , e.getMessage());
-    }
+     // todo: implement it
 }
