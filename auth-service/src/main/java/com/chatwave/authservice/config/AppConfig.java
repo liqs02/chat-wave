9bebd8f5aa8f395d8e5c7369b6ecc2d7852edb60
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-24 19:41:44 +0200

    create AuthenticationProvider test (AppConfig)

--- auth-service/src/main/java/com/chatwave/authservice/config/AppConfig.java
+++ auth-service/src/main/java/com/chatwave/authservice/config/AppConfig.java
@@ -2,15 +2,27 @@ package com.chatwave.authservice.config;
 
 import com.chatwave.authservice.domain.UserMapper;
 import com.chatwave.authservice.domain.session.SessionMapper;
+import com.chatwave.authservice.repository.UserRepository;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static java.lang.Integer.parseInt;
+
 @Configuration
+@Setter(onMethod_=@Autowired)
 public class AppConfig {
+    private UserRepository userRepository;
+
     @Bean
     public UserMapper userMapper() {
         return UserMapper.INSTANCE;
@@ -29,5 +41,19 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return user -> userRepository.findById(parseInt(user))
+                .orElseThrow(() ->  new UsernameNotFoundException("User with given id does not exist."));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
---
+++ auth-service/src/test/java/com/chatwave/authservice/config/AppConfigTest.java
@@ -0,0 +1,47 @@
+package com.chatwave.authservice.config;
+
+import com.chatwave.authservice.domain.User;
+import com.chatwave.authservice.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@DisplayName("AppConfig")
+public class AppConfigTest {
+    @Autowired
+    AuthenticationProvider authenticationProvider;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    @DisplayName("AuthenticationProvider test")
+    public void t1() {
+        var user = new User();
+        user.setId(1);
+
+        var encoded = passwordEncoder.encode("pass");
+        user.setPassword(encoded);
+
+        userRepository.save(user);
+
+        var result = authenticationProvider.authenticate(
+                new UsernamePasswordAuthenticationToken(1, "pass")
+        );
+
+
+        var principal = (User) result.getPrincipal();
+        assertEquals(1, principal.getId());
+        assertEquals(encoded, principal.getPassword());
+
+        assertEquals("pass", result.getCredentials());
+    }
+}
