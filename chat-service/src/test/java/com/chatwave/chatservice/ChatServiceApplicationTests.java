c0574c6a47cf72b9dbe1a5b6e137d2663c4a4a28
Patrick <90937664+blackydev@users.noreply.github.com>
2023-07-31 21:35:05 +0200

    create chat-service

---
+++ chat-service/src/test/java/com/chatwave/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatwave.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
