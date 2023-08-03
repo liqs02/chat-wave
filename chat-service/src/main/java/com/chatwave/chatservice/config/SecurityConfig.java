a903c714e90158fe33c25b819e3063993a719daa
Patrick <90937664+blackydev@users.noreply.github.com>
2023-08-03 13:16:42 +0200

    create SecurityConfig in chat-service

---
+++ chat-service/src/main/java/com/chatwave/chatservice/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.chatwave.chatservice.config;
+
+import com.chatwave.authclient.filter.UserAuthFilter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@Configuration
+@EnableWebSecurity
+@Setter(onMethod_=@Autowired)
+public class SecurityConfig {
+    UserAuthFilter userAuthFilter;
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                    auth.requestMatchers(GET, "/actuator/health").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .anyRequest().authenticated()
+            )
+            .addFilterBefore(userAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
