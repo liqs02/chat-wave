7b341dd696bf1fc4a0942bce9ae2f2af1a3aa86a
Patrick <90937664+blackydev@users.noreply.github.com>
2023-08-03 13:12:14 +0200

    create ChatService with tests

--- chat-service/src/main/java/com/chatwave/chatservice/config/AppConfig.java
+++ chat-service/src/main/java/com/chatwave/chatservice/config/AppConfig.java
@@ -3,11 +3,17 @@ package com.chatwave.chatservice.config;
 import com.chatwave.chatservice.domain.MessageMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 
 @Configuration
 public class AppConfig {
     @Bean
     MessageMapper messageMapper() {
         return MessageMapper.INSTANCE;
+    }
+
+    @Bean
+    Pageable chatPageable() {
+        return Pageable.ofSize(10);
     }
 }
