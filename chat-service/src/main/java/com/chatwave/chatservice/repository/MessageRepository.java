db04c2a1ea878607e2c5ca9b866a996eee31946b
Patrick <90937664+blackydev@users.noreply.github.com>
2023-08-03 12:34:01 +0200

    add findChat() method to MessageRepository with tests

--- chat-service/src/main/java/com/chatwave/chatservice/repository/MessageRepository.java
+++ chat-service/src/main/java/com/chatwave/chatservice/repository/MessageRepository.java
@@ -1,8 +1,15 @@
 package com.chatwave.chatservice.repository;
 
 import com.chatwave.chatservice.domain.Message;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface MessageRepository extends JpaRepository<Message, UUID> {}
+public interface MessageRepository extends JpaRepository<Message, UUID> {
+    @Query("SELECT m FROM Message m WHERE (m.authorId = ?1 AND m.receiverId = ?2) OR (m.authorId = ?2 AND m.receiverId = ?1) ORDER BY m.createdAt DESC")
+    List<Message> findChat(Integer firstMemberId, Integer secondMemberId, Pageable pageable);
+
+}
