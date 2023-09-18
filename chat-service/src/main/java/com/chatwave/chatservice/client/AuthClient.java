f2ea9dafd8160a469272ba55f2b53018a5b50251
Patrick <90937664+blackydev@users.noreply.github.com>
2023-09-18 17:01:04 +0200

    add WireMockServer to chat-service tests

--- chat-service/src/main/java/com/chatwave/chatservice/client/AuthClient.java
+++ chat-service/src/main/java/com/chatwave/chatservice/client/AuthClient.java
@@ -2,5 +2,5 @@ package com.chatwave.chatservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient("auth-service")
+@FeignClient(name = "auth-service", url = "${auth-service.url:}")
 public interface AuthClient extends com.chatwave.authclient.client.AuthClient {}
